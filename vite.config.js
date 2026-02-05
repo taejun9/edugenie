b771a4db2518ff09236194e86581d72a073b9cb9
taejun9 <kysktj123@gmail.com>
2026-02-05 13:54:53 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,19 @@
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export default defineConfig(({ command, mode }) => {
-  if (command === 'serve') {
-    return {
-      plugins: [vue()],
-      server: {
-        host: '0.0.0.0',
-        https: {
-          key: fs.readFileSync('./.cert/key.pem'),
-          cert: fs.readFileSync('./.cert/cert.pem'),
-        },
-        hmr: {
-          host: 'local.github.io',
-        },
-      },
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-      },
-    }
-  } else {
-    // command === 'build'
-    return {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-      },
-      build: {
-        chunkSizeWarningLimit: 1600,
-      },
-      base: '/edugenie/',
-    }
-  }
+export default defineConfig({
+  base: '/edugenie/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
